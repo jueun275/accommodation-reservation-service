0e50d2023d67e24b763b969df754c7407bbb9dd4
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-05-04 23:36:38 +0900

    chore: data.sql 추가
    - 테스트용 user 정보 넣어놓기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, password, name, phone, role)
+VALUES
+    ('user1@example.com', 'encoded-password-1', '일반유저', '010-1234-5678', 'USER'),
+    ('owner1@example.com', 'encoded-password-2', '숙소주인', '010-8765-4321', 'OWNER');
